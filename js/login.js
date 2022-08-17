46b5a141229983b0beccd973f1603f31353d6d86
alfogrip <alfogrip@gmail.com>
2022-08-17 11:42:38 -0300

    Se editó

--- js/login.js
+++ js/login.js
@@ -1,14 +1,9 @@
 document.addEventListener("DOMContentLoaded",function(){
 
-    document.addEventListener("submit",function(event){
-        let emailUser = document.getElementById("email").value;
-        let passUser = document.getElementById("pass").value;
-        localStorage.setItem("email",emailUser);
-        localStorage.setItem("password",passUser);
+    document.getElementById("form-login").addEventListener("submit",function(event){
         event.preventDefault();
-        // window.location.href = "https://alfogrip.github.io/proyectojap/"
-        
-    
+        window.location.href = "home.html";
+
     });
 
 });
