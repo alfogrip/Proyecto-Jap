283d65c74ac683bbb8fe4c63189f3a8ce22b103c
alfogrip <alfogrip@gmail.com>
2022-08-19 12:28:55 -0300

    Se completó el login

--- js/login.js
+++ js/login.js
@@ -1,8 +1,27 @@
 document.addEventListener("DOMContentLoaded",function(){
 
+    let userEmail = document.getElementById("email");
+    let userPass = document.getElementById("pass");
+    let pEmail = "Ingrese un email";
+    let pPass = "Ingrese una contraseña";
+
     document.getElementById("form-login").addEventListener("submit",function(event){
         event.preventDefault();
-        window.location.href = "home.html";
+        
+        if (userEmail.value == "" && userPass.value == ""){
+            userEmail.style.border = "red solid 1px";
+            userPass.style.border = "red solid 1px";
+            document.getElementById("p-email").innerHTML = pEmail;
+            document.getElementById("p-pass").innerHTML = pPass;
+        } else if (userEmail.value == ''){
+            userEmail.style.border = "red solid 1px";
+            document.getElementById("p-email").innerHTML = pEmail;
+        } else if(userPass.value == ''){
+            userPass.style.border = "red solid 1px";
+            document.getElementById("p-pass").innerHTML = pPass;
+        } else{
+            window.location.href = "home.html";
+        }
 
     });
 
