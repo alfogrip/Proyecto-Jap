93e5af26761d47a8fa7c651f3b6442e987d6a84c
alfogrip <alfogrip@gmail.com>
2022-08-25 22:37:02 -0300

    Se agregaron filtros a products

--- js/products.js
+++ js/products.js
@@ -1,3 +1,8 @@
+const ORDER_DESC_BY_PRICE = "DESC";
+const ORDER_ASC_BY_PRICE = "ASC";
+const ORDER_DESC_BY_PROD_COUNT = "Rel";
+let currentSortCriteria = undefined;
+let currentProductsArray = [];
 let productsArray = [];
 let userEmail = localStorage.getItem("userEmail");
 let catID = localStorage.getItem("catID");
@@ -5,6 +10,32 @@ let PRODUCTS_ID_URL = `${PRODUCTS_URL}${catID}${EXT_TYPE}`;
 
 // Agrega el email al encabezado.
 document.getElementById("profile").innerHTML = `${userEmail}`;
+
+function sortProducts(criteria, array){
+    let result = [];
+    if (criteria === ORDER_DESC_BY_PRICE){
+        result = array.sort(function(a,b){
+            if (a.cost > b.cost){return -1;}
+            if (a.cost < b.cost){return 1;}
+            return 0;
+        });
+    }
+    else if (criteria === ORDER_ASC_BY_PRICE){
+        result = array.sort(function(a,b){
+            if (a.cost < b.cost){return -1;}
+            if (a.cost > b.cost){return 1;}
+            return 0;
+        });
+    }
+    else if(criteria === ORDER_DESC_BY_PROD_COUNT){
+        result = array.sort(function(a,b){
+            if (a.soldCount > b.soldCount){return -1;}
+            if (a.soldCount < b.soldCount){return 1;}
+            return 0;
+        });
+    }
+    return result;
+};
 
 // Función que despliega una lista con los productos correspondientes a su categoría.
 function showProducts(array){
@@ -30,10 +61,19 @@ function showProducts(array){
             </div>`
     }
     document.getElementById("product-list").innerHTML = htmlContent;
- }
+ };
+
+ function sortAndShowProducts(sortCriteria, productsArray){
+    currentSortCriteria = sortCriteria;
+    if(productsArray != undefined){
+        currentProductsArray = productsArray;
+    };
+    currentProductsArray = sortProducts(currentSortCriteria, currentProductsArray);
+    showProducts(currentProductsArray);
+};
 
 document.addEventListener("DOMContentLoaded", function(){
-
+    
     getJSONData(PRODUCTS_ID_URL).then(objProduct => {
         if (objProduct.status === "ok") {
             productsArray = objProduct.data.products;
@@ -42,4 +82,18 @@ document.addEventListener("DOMContentLoaded", function(){
             showProducts(productsArray);
         }
     });
+
+    document.getElementById("sortDescByPrice").addEventListener("click",function(){
+        sortAndShowProducts(ORDER_DESC_BY_PRICE, productsArray);
+        
+    });
+    document.getElementById("sortAscByPrice").addEventListener("click",function(){
+        sortAndShowProducts(ORDER_ASC_BY_PRICE, productsArray);
+        
+    });
+    document.getElementById("sortDescByCount").addEventListener("click",function(){
+        sortAndShowProducts(ORDER_DESC_BY_PROD_COUNT, productsArray);
+        
+    });
+
 });
